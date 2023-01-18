a8d9bd270a2e9934d8e1e031bef119c3f9dc7f51
Peter Szilvasi <peti.szilvasi95@gmail.com>
2023-01-18 17:46:51 +0100

    Extract the dataset

---
+++ 02CloudDataWarehouse/project-solution/README.md
@@ -0,0 +1,9 @@
+# Cloud Data Warehouses with Azure
+
+## Extract data
+
+Extract the bike sharing dataset from `../project-starter/azure-data-warehouse-projectdatafiles.zip` to the folder called `data`.
+
+```
+python extract_zip.py
+```
---
+++ 02CloudDataWarehouse/project-solution/extract_zip.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+import re
+
+from zipfile import ZipFile
+
+
+def unzip(zipfile, extract_to):
+    with ZipFile(zipfile, "r") as zip_file:
+        for file in zip_file.namelist():
+            # match only for the csv files under the 'Out/' folder
+            if not re.search('^Out\/\w+\.csv$', file):
+                continue
+            
+            # create the new destination directory
+            if not os.path.exists(extract_to):
+                os.makedirs(extract_to)
+
+            filename = os.path.basename(file)
+
+            # copy file without its folder structure
+            source = zip_file.open(file)
+            target = open(os.path.join(extract_to, filename), "wb")
+            with source, target:
+                shutil.copyfileobj(source, target)
+
+
+if __name__ == "__main__":
+    zipfile = '../project-starter/azure-data-warehouse-projectdatafiles.zip'
+    extract_to = './data'
+    unzip(zipfile, extract_to)
