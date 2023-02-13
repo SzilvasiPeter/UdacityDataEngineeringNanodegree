705eea47d5fa5860e80c0ad3788e53c15fd27d52
Szilvasi Peter BDP EZD <peti.szilvasi95@gmail.com>
2023-02-13 10:49:10 +0100

    Add helper SQL script file

---
+++ 04DataPipeline/project-starter/sql_scripts.sql
@@ -0,0 +1,53 @@
+CREATE TABLE [dbo].[NYC_Payroll_Data](
+    [FiscalYear] [int] NULL,
+    [PayrollNumber] [int] NULL,
+    [AgencyID] [varchar](10) NULL,
+    [AgencyName] [varchar](50) NULL,
+    [EmployeeID] [varchar](10) NULL,
+    [LastName] [varchar](20) NULL,
+    [FirstName] [varchar](20) NULL,
+    [AgencyStartDate] [date] NULL,
+    [WorkLocationBorough] [varchar](50) NULL,
+    [TitleCode] [varchar](10) NULL,
+    [TitleDescription] [varchar](100) NULL,
+    [LeaveStatusasofJune30] [varchar](50) NULL,
+    [BaseSalary] [float] NULL,
+    [PayBasis] [varchar](50) NULL,
+    [RegularHours] [float] NULL,
+    [RegularGrossPaid] [float] NULL,
+    [OTHours] [float] NULL,
+    [TotalOTPaid] [float] NULL,
+    [TotalOtherPay] [float] NULL
+) 
+GO
+
+CREATE TABLE [dbo].[NYC_Payroll_EMP_MD](
+    [EmployeeID] [varchar](10) NULL,
+    [LastName] [varchar](20) NULL,
+    [FirstName] [varchar](20) NULL
+) 
+GO
+
+CREATE TABLE [dbo].[NYC_Payroll_TITLE_MD](
+    [TitleCode] [varchar](10) NULL,
+    [TitleDescription] [varchar](100) NULL
+) 
+GO
+
+CREATE TABLE [dbo].[NYC_Payroll_AGENCY_MD](
+    [AgencyID] [varchar](10) NULL,
+    [AgencyName] [varchar](50) NULL
+) 
+GO
+
+CREATE TABLE [dbo].[NYC_Payroll_Summary](
+    [FiscalYear] [int] NULL,
+    [AgencyName] [varchar](50) NULL,
+    [TotalPaid] [float] NULL
+)
+
+-- Filter activity after UNION
+toInteger(FiscalYear) >= $dataflow_param_fiscalyear
+
+-- Total paid column
+RegularGrossPaid + TotalOTPaid+TotalOtherPay
